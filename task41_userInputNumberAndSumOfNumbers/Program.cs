1d3a6dad59c3875009f233d1fd9dd1563a5bd356
Yegor <ukstudio.yegor@mail.ru>
2023-07-23 17:59:40 +0600

    homework is done 23.07

--- task41_userInputNumberAndSumOfNumbers/Program.cs
+++ task41_userInputNumberAndSumOfNumbers/Program.cs
@@ -2,3 +2,44 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+int EnterValueInt (string text)
+{
+    Console.Write(text);
+    int value = Convert.ToInt32(Console.ReadLine());
+
+    return value;
+}
+
+int[] FillArray (int limit)
+{
+    int [] array = new int [limit];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = EnterValueInt($"Введите {i+1} число в массиве -> ");
+
+    }
+
+    return array;
+}
+
+int Counter (int [] array)
+{
+    int counter = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0) counter++;
+    }
+
+    return counter;
+}
+
+int n = EnterValueInt("Введите количество элементов массива -> ");
+
+int[] array = FillArray(n);
+
+int counter = Counter(array);
+
+Console.WriteLine($"В массиве [{string.Join(", ", array)}] -> {counter} чисел больше нуля");
+
--- task43_twoLines/Program.cs
+++ task43_twoLines/Program.cs
@@ -1,3 +1,23 @@
 ﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double EnterValueDouble (string text)
+{
+    Console.Write(text);
+    double value = Convert.ToDouble(Console.ReadLine());
+
+    return value;
+}
+
+double b1 = EnterValueDouble("Ввведите b1 -> ");
+double k1 = EnterValueDouble("Ввведите k1 -> ");
+double b2 = EnterValueDouble("Ввведите b2 -> ");
+double k2 = EnterValueDouble("Ввведите k2 -> ");
+
+double x = (b2 - b1) / (k1 - k2);
+
+double y1 = (k1*x) + b1;
+double y2 = (k2*x) + b2;
+
+Console.WriteLine($"({y1}, {y2})");
